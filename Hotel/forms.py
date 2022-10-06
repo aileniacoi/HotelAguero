e36b294ede62d41fabea847c518bffdc7429f5d3
Rodrigokbr4 <cabrerarodrigo481@gmail.com>
2022-10-05 22:12:12 -0300

    avances en la vista detalle de reservas tambien en el archivo "reservasForm.html" y en "forms.py"

--- Hotel/forms.py
+++ Hotel/forms.py
@@ -18,3 +18,15 @@ class ReservaForm(forms.ModelForm):
     class Meta:
         model = Reserva
         fields = "__all__"
+        widgets = {
+            'fechaIngreso' : forms.TextInput(attrs={'class': 'form-control'}),
+            'fechaEgreso' : forms.TextInput(attrs={'class': 'form-control'}),
+            'cantidadPersonas' : forms.TextInput(attrs={'class': 'form-control'}),
+            'idHabitacion' : forms.TextInput(attrs={'class': 'form-control'}),
+            'idCliente' : forms.TextInput(attrs={'class': 'form-control'}),
+            'senia' : forms.TextInput(attrs={'class': 'form-control'}),
+            'precioTotal' : forms.TextInput(attrs={'class': 'form-control'}),
+            'precioPorDia' : forms.TextInput(attrs={'class': 'form-control'}),
+            'incluyeDesayuno' : forms.TextInput(attrs={'class': 'form-control'}),
+            'observaciones' : forms.TextInput(attrs={'class': 'form-control'}),
+        }
