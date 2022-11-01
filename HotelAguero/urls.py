50203034a151a1e0688eeaf5274993476a8dcce1
Rodrigokbr4 <cabrerarodrigo481@gmail.com>
2022-10-31 21:35:42 -0300

    probando la autentificacion (no de la manera correcta!)

--- HotelAguero/urls.py
+++ HotelAguero/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from HotelAguero.views import profile
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/', profile, name='profile'),
+    path('admin/', admin.site.urls), path('', include('Hotel.urls')),
     path('', include('Hotel.urls'))
 ]
