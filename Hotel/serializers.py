ad88443e8220b4d87e0068da55fc3981866ca635
Ailen Iacoi <aileniacoi@gmail.com>
2023-02-26 02:17:08 -0300

    Se agregó popover y ajustes de colores segun estado en calendario de reservas

--- Hotel/serializers.py
+++ Hotel/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Cliente, Habitacion, Reserva
+from .models import Cliente, Habitacion, Reserva, MovimientoCaja
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,9 +11,34 @@ class HabitacionSerializer(serializers.ModelSerializer):
         model = Habitacion
         fields = ('pk', 'numero', 'plazas')
 
+class CajaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MovimientoCaja
+        fields = ('pk', 'monto')
+
 class ReservaSerializer(serializers.ModelSerializer):
+    total_pagos = serializers.SerializerMethodField()
+    fechaIngreso = serializers.SerializerMethodField()
+    fechaEgreso = serializers.SerializerMethodField()
+    fechaRegistro = serializers.SerializerMethodField()
+
     idCliente = ClienteSerializer()
     idHabitacion = HabitacionSerializer()
     class Meta:
         model = Reserva
-        fields = ('pk', 'fechaIngreso', 'fechaEgreso', 'idHabitacion', 'seniaSolicitada', 'idCliente')
+        fields = ('pk', 'fechaIngreso', 'fechaEgreso', 'idHabitacion', 'precioTotal', 'idCliente', 'cantidadPersonas',
+                  'total_pagos', 'fechaRegistro')
+
+    def get_total_pagos(self, obj):
+        pagos = MovimientoCaja.objects.filter(idReserva=obj)
+        total = sum(pago.monto for pago in pagos)
+        return total
+
+    def get_fechaIngreso(self, obj):
+        return obj.fechaIngreso.strftime('%d/%m/%Y')
+
+    def get_fechaEgreso(self, obj):
+        return obj.fechaEgreso.strftime('%d/%m/%Y')
+
+    def get_fechaRegistro(self, obj):
+        return obj.fechaRegistro.strftime('%d/%m/%Y')
