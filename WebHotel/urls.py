b0999e53e941bf14a1a6ed9ce857b3737c42fae7
Rodrigo Cabrera <rodrigo.cabrera@incluit.com>
2023-03-09 19:51:05 -0300

    se añade galeria

--- WebHotel/urls.py
+++ WebHotel/urls.py
@@ -6,4 +6,6 @@ from WebHotel.views import inicio2
 urlpatterns = [
     path('inicio2', views.inicio2, name='inicio2'),
     path('index2', views.index2, name='index2'),
+    path('conocenos', views.Conocenos, name='conocenos'),
+    path('galeria', views.Galeria, name='galeria'),
 ]
